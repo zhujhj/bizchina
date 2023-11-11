651e9a88e3e4cea77f584af5c6c907cf0b195c3e
Johnlman <johnlman1@yahoo.com>
2023-11-11 03:42:08 -0800

    Made some progress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import  React, { useRef, useState } from 'react';
 import './App.css';
 
 import './firebaseConfig.js';
@@ -10,10 +10,10 @@ import 'firebase/compat/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+import { getAuth, createUserWithEmailAndPassword,signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 
-
-const auth = firebase.auth();
+const auth = getAuth();
 const firestore = firebase.firestore();
 const analytics = firebase.analytics();
 
@@ -38,17 +38,69 @@ function App() {
 }
 
 function SignIn() {
-
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const signInWithGoogle = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
-        auth.signInWithPopup(provider);
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
     }
+    const signUpWithEmailAndPassword = () => {
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed up
+                const user = userCredential.user;
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
 
+    };
     return (
-        <>
+        <div id="signInContainer">
+            <div id="signIn">Sign in</div>
+            <div id="manage">Log in to start managing your tasks!</div>
+
+            {/* Email and Password inputs */}
+            <input
+                type="text"
+                placeholder="Email"
+                className="inputBox"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                className="inputBox"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+
+            {/* Sign in buttons */}
+            <button id="login" onClick={() => signUpWithEmailAndPassword(email, password)}>Login</button>
             <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-            <p>Do not violate the community guidelines or you will be banned for life!</p>
-        </>
+        </div>
     )
 
 }
